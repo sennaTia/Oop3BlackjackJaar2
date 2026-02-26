24d09f720542c9bff65c87fcc5bb461443cb7d10
sennaTia <1214998@student.roc-nijmegen.nl>
2026-02-26 10:44:47 +0100

    Add project files.

---
+++ Oop3BlackjackJaar2/Form1.cs
@@ -0,0 +1,15 @@
+namespace Oop3BlackjackJaar2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
