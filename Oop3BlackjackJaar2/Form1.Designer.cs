b962abdd501e5079df1562665a8b04e1182cae8b
sennaTia <1214998@student.roc-nijmegen.nl>
2026-02-26 10:49:50 +0100

    initial commit

--- Oop3BlackjackJaar2/Form1.Designer.cs
+++ Oop3BlackjackJaar2/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(0, 0);
+            button1.Location = new Point(1, 0);
             button1.Name = "button1";
             button1.Size = new Size(131, 40);
             button1.TabIndex = 0;
