e7c226745ec877149843cd4e20c5ed83805ac231
sennaTia <1214998@student.roc-nijmegen.nl>
2026-03-13 12:43:36 +0100

    mini commit

--- Oop3BlackjackJaar2/Form1.Designer.cs
+++ Oop3BlackjackJaar2/Form1.Designer.cs
@@ -51,6 +51,7 @@
             radioButton1.TabStop = true;
             radioButton1.Text = "radioButton1";
             radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
             // 
             // Form1
             // 
--- Oop3BlackjackJaar2/Form1.cs
+++ Oop3BlackjackJaar2/Form1.cs
@@ -11,5 +11,10 @@ namespace Oop3BlackjackJaar2
         {
 
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
