1b564d71684abc437e729dc4a9a99eabb9e6afb0
sennaTia <1214998@student.roc-nijmegen.nl>
2026-03-26 11:16:15 +0100

    Starting to develop

--- Oop3BlackjackJaar2/Form1.Designer.cs
+++ Oop3BlackjackJaar2/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,37 +23,88 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support.
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnStand = new Button();
+            btnHit = new Button();
+            btnStart = new Button();
+            lblPlayer = new Label();
+            lblDealer = new Label();
             SuspendLayout();
             // 
-            // button1
+            // btnStand
             // 
-            button1.Location = new Point(0, 0);
-            button1.Name = "button1";
-            button1.Size = new Size(131, 40);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnStand.Location = new Point(756, 260);
+            btnStand.Name = "btnStand";
+            btnStand.Size = new Size(131, 40);
+            btnStand.TabIndex = 0;
+            btnStand.Text = "Stand";
+            btnStand.UseVisualStyleBackColor = true;
+            btnStand.Click += btnStand_Click;
+            // 
+            // btnHit
+            // 
+            btnHit.Location = new Point(430, 260);
+            btnHit.Name = "btnHit";
+            btnHit.Size = new Size(131, 40);
+            btnHit.TabIndex = 1;
+            btnHit.Text = "Hit";
+            btnHit.UseVisualStyleBackColor = true;
+            btnHit.Click += btnHit_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(106, 260);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(131, 40);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // lblPlayer
+            // 
+            lblPlayer.AutoSize = true;
+            lblPlayer.Location = new Point(212, 453);
+            lblPlayer.Name = "lblPlayer";
+            lblPlayer.Size = new Size(92, 30);
+            lblPlayer.TabIndex = 3;
+            lblPlayer.Text = "Speler: 0";
+            // 
+            // lblDealer
+            // 
+            lblDealer.AutoSize = true;
+            lblDealer.Location = new Point(631, 453);
+            lblDealer.Name = "lblDealer";
+            lblDealer.Size = new Size(95, 30);
+            lblDealer.TabIndex = 4;
+            lblDealer.Text = "Dealer: 0";
+            lblDealer.Click += lblDealer_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(1067, 760);
+            Controls.Add(lblDealer);
+            Controls.Add(lblPlayer);
+            Controls.Add(btnStart);
+            Controls.Add(btnHit);
+            Controls.Add(btnStand);
             Name = "Form1";
-            Text = "Form1";
-            Click += Form1_Click;
+            Text = "Blackjack";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnStand;
+        private Button btnHit;
+        private Button btnStart;
+        private Label lblPlayer;
+        private Label lblDealer;
     }
 }
--- Oop3BlackjackJaar2/Form1.cs
+++ Oop3BlackjackJaar2/Form1.cs
@@ -1,18 +1,96 @@
+using System;
+using System.Windows.Forms;
+
 namespace Oop3BlackjackJaar2
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        int playerScore = 0;
+        int dealerScore = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Click(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e)
         {
+            playerScore = 0;
+            dealerScore = 0;
 
+            lblPlayer.Text = "Speler: 0";
+            lblDealer.Text = "Dealer: 0";
+
+            playerScore += TrekKaart();
+            playerScore += TrekKaart();
+
+            lblPlayer.Text = "Speler: " + playerScore;
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            playerScore += TrekKaart();
+            lblPlayer.Text = "Speler: " + playerScore;
+
+            if (playerScore > 21)
+            {
+                MessageBox.Show("Je bent over 21. Je hebt verloren.");
+            }
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            while (dealerScore < 17)
+            {
+                dealerScore += TrekKaart();
+            }
+
+            lblDealer.Text = "Dealer: " + dealerScore;
+
+            if (dealerScore > 21)
+            {
+                MessageBox.Show("Dealer is over 21. Jij wint.");
+            }
+            else if (playerScore > dealerScore)
+            {
+                MessageBox.Show("Jij wint.");
+            }
+            else if (playerScore < dealerScore)
+            {
+                MessageBox.Show("Dealer wint.");
+            }
+            else
+            {
+                MessageBox.Show("Gelijkspel.");
+            }
+        }
+
+        private int TrekKaart()
+        {
+            string[] kaarten = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            string kaart = kaarten[rnd.Next(kaarten.Length)];
+
+            int waarde;
+
+            if (kaart == "A")
+            {
+                waarde = 11;
+            }
+            else if (kaart == "J" || kaart == "Q" || kaart == "K")
+            {
+                waarde = 10;
+            }
+            else
+            {
+                waarde = int.Parse(kaart);
+            }
+
+            MessageBox.Show("Je trok: " + kaart);
+            return waarde;
+        }
+
+        private void lblDealer_Click(object sender, EventArgs e)
         {
 
         }
